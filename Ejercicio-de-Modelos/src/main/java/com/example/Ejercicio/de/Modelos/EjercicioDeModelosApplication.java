9e0212d46a9c8550b33707ca718a95b6abe3ef0a
ale499 <gimenezalejandro083@gmail.com>
2024-09-02 22:39:24 -0300

    primer commit

---
+++ Ejercicio-de-Modelos/src/main/java/com/example/Ejercicio/de/Modelos/EjercicioDeModelosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ejercicio.de.Modelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioDeModelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioDeModelosApplication.class, args);
+	}
+
+}
---
+++ Ejercicio-de-Modelos/src/test/java/com/example/Ejercicio/de/Modelos/EjercicioDeModelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ejercicio.de.Modelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioDeModelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

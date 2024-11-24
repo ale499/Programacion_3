e025e9f6182da6262e7af7ed17f52be0f5d77b33
FL1XEvac <Altamira2004@outlook.com>
2024-11-23 22:44:57 -0300

    Trabajos Practicos Terminados

--- Ejercicio-de-Modelos/src/main/java/com/example/Ejercicio/de/Modelos/EjerciciodeModelosApplication.java
+++ Ejercicio-de-Modelos/src/main/java/com/example/Ejercicio/de/Modelos/EjerciciodeModelosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EjercicioDeModelosApplication {
+public class EjerciciodeModelosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EjercicioDeModelosApplication.class, args);
+		SpringApplication.run(EjerciciodeModelosApplication.class, args);
 	}
 
 }
